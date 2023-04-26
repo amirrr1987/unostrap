bb396ca513f9d4ec968606f0f6ce4a4115adad0c
Amir Maghami <Maghami.A1987@gmail.com>
2023-04-26 13:58:44 +0430

    feat: footer and card

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import Button from './src/Button'
+import Card from './src/Card'
+import Footer from './src/Footer'
 import Navbar from './src/Navbar'
 
-export { Button, Navbar }
+export {
+    Button,
+    Card,
+    Footer,
+    Navbar
+}
